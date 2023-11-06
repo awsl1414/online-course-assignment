54cca83ac6f647ac71c23c8493c1edcc685144cc
awsl1414 <3030994569@qq.com>
2023-11-06 19:57:03 +0800

    添加机房楼层相关查询

--- backend/api/v1/router_computer_room.py
+++ backend/api/v1/router_computer_room.py
@@ -3,7 +3,7 @@ from typing import List, Optional
 from sqlalchemy.orm import Session
 
 from core import get_db
-from crud import get_computer_room_software
+from crud import get_computer_room_software, get_computer_room_floor
 
 from schemas import Response200, Response400, OriginClassIn
 
@@ -25,3 +25,9 @@ def get_computer_room_software_api(
     results = {"software": item[0] for item in result}
     results.update({"code": 200, "msg": "请求成功"})
     return results
+
+
+@router_computer_room.get("/get_computer_room_floor", summary="机房楼层相关查询")
+def get_computer_room_floor_api(db: Session = Depends(get_db)):
+    result = get_computer_room_floor(db=db)
+    return {"code": 200, "msg": "请求成功", "floor": result}
