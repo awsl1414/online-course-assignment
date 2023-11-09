9f90eba45ed4a6c5a6f9c35499b80f34adae3f6f
awsl1414 <3030994569@qq.com>
2023-11-09 09:40:08 +0800

    临时存储

--- backend/schemas/schemas_main_class.py
+++ backend/schemas/schemas_main_class.py
@@ -3,19 +3,19 @@ from pydantic import BaseModel
 
 
 class MainClassIn(BaseModel):
-    # teacherName: str
-    # teacherRoom: str
-    # courseName: str
-    # className: str
-    # population: int
-    # software: str
-    # computerRoomName: str
-    # week: int
-    # weekDay: int
-    # lesson: str
-    # littleLesson: str
-    # cycle: str
-    data: List[str]
+    teacherName: str
+    teacherRoom: str
+    courseName: str
+    className: str
+    population: int
+    software: str
+    computerRoomName: str
+    week: int
+    weekDay: int
+    lesson: str
+    littleLesson: str
+    cycle: str
+    # data: List[str]
 
     # class Config:
     #     json_schema_extra = {
